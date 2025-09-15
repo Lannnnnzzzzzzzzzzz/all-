52ba4002de1aa7899fd2b352b192b8e239e214bf
itsmelann <168535741+Lannnnnzzzzzzzzzzz@users.noreply.github.com>
2025-09-15 12:47:09 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,26 @@ export default function Home() {
   return (
     <Layout>
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-        <Link href="/anime"><a className="block p-8 bg-gradient-to-br from-blue-500 to-indigo-600 text-white rounded-lg text-center">Anime</a></Link>
-        <Link href="/donghua"><a className="block p-8 bg-gradient-to-br from-red-500 to-pink-600 text-white rounded-lg text-center">Donghua</a></Link>
-        <Link href="/comic"><a className="block p-8 bg-gradient-to-br from-green-500 to-emerald-600 text-white rounded-lg text-center">Comic</a></Link>
+        <Link
+          href="/anime"
+          className="block p-8 bg-gradient-to-br from-blue-500 to-indigo-600 text-white rounded-lg text-center"
+        >
+          Anime
+        </Link>
+
+        <Link
+          href="/donghua"
+          className="block p-8 bg-gradient-to-br from-red-500 to-pink-600 text-white rounded-lg text-center"
+        >
+          Donghua
+        </Link>
+
+        <Link
+          href="/comic"
+          className="block p-8 bg-gradient-to-br from-green-500 to-emerald-600 text-white rounded-lg text-center"
+        >
+          Comic
+        </Link>
       </div>
     </Layout>
   )
